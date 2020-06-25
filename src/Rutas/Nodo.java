a9b32638cbb82cb045c29efa12db9a8e2e044922
TheNewdabs <57052205+TheNewdabs@users.noreply.github.com>
2020-06-25 16:34:38 -0500

    Paquete Rutas Finalizado
    Se completaron y cargaron todos los metodos de las clases del Paquete Rutas - 201807100 - Daniel Barillas

--- src/Rutas/Nodo.java
+++ src/Rutas/Nodo.java
@@ -1,5 +1,54 @@
 //@author Dabs
 package Rutas;
 public class Nodo {
+    private String Nombre;
+    private int Tiempo;
+    private Nodo Siguiente;
+    private Lista Caminos;
     
+    public Nodo(String Nombre){
+        this.Nombre = Nombre;
+        Tiempo = 0;
+        Siguiente = null;
+        Caminos = new Lista();
+    }
+    
+    public Nodo(String Nombre, int Tiempo){
+        this.Nombre = Nombre;
+        this.Tiempo = Tiempo;
+        Siguiente = null;
+        Caminos = new Lista();
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public int getTiempo() {
+        return Tiempo;
+    }
+
+    public void setTiempo(int Tiempo) {
+        this.Tiempo = Tiempo;
+    }
+
+    public Nodo getSiguiente() {
+        return Siguiente;
+    }
+
+    public void setSiguiente(Nodo Siguiente) {
+        this.Siguiente = Siguiente;
+    }
+
+    public Lista getCaminos() {
+        return Caminos;
+    }
+
+    public void setCaminos(Lista Caminos) {
+        this.Caminos = Caminos;
+    }
 }
