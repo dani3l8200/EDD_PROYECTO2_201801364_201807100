f3159043894d06568b2200999e2c2e9c469f5fea
dani0l8200 <dani3l25948@gmail.com>
2020-06-26 03:40:04 +0000

    [201801364]metodo para carga masiva de archivos clientes y vehiculos

--- src/edd_proyecto2_201801364_201807100/EDD_PROYECTO2_201801364_201807100.java
+++ src/edd_proyecto2_201801364_201807100/EDD_PROYECTO2_201801364_201807100.java
@@ -4,18 +4,21 @@
  * and open the template in the editor.
  */
 package edd_proyecto2_201801364_201807100;
+import Customers.HashTable;
+import Vehicle.*;
 
 /**
  *
  * @author dani3l8200 and Dabs
  */
 public class EDD_PROYECTO2_201801364_201807100 {
-
+public static HashTable hashtTable = new HashTable();
+public static BTree<Vehicle> tree = new BTree<Vehicle>();
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         // TODO code application logic here
-    }
+    }*/
     
 }
